6b9176b7695f08832e30f3d8f3b316883e98c2c7
Jesus D. Flores <danyflores052@gmail.com>
2019-11-10 09:03:45 -0800

    add more jscript
    adding notes

--- jsxz2/JS/javascript.js
+++ jsxz2/JS/javascript.js
@@ -1,5 +1,20 @@
-newH1 = document.createElement("h1");
+// newH1 = document.createElement("h1");
 
-newH1.textContent = "hi =)";
+// newH1.textContent = "hi =)";
 
-document.body.appendChild(newH1);
+// document.body.appendChild(newH1);
+
+
+// returns data type of an object - evaluates to an object
+console.log(document.getElementById('div-two'));
+
+// returns an array with objects calls called HTML collection - evaluates to HTML collection
+console.log(document.getElementsByClassName('box'));
+
+// adding a specific index will return an element object
+console.log(document.getElementsByClassName('box')[1]);
+
+
+// returns html collection of all elements with specific tag name as an HTML collection
+console.log(document.getElementsByTagName('div'));
+
