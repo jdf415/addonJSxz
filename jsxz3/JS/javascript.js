cc900f7c1167d107b64c48eb84a64fa1194ac9c4
Jesus D. Flores <danyflores052@gmail.com>
2019-11-10 12:00:13 -0800

    updateJSXZ3
    adding notes, and fucntions

--- jsxz3/JS/javascript.js
+++ jsxz3/JS/javascript.js
@@ -1,6 +1,57 @@
-newH1 = document.createElement("h1");
+// newH1 = document.createElement("h1");
 
-newH1.textContent = "hi =)";
+// newH1.textContent = "hi =)";
 
-document.getElementById("hello").appendChild(newH1);
+// document.getElementById("hello").appendChild(newH1);
 
+
+
+
+// .length is a static number
+// array is an object
+var numArray = [1,2,3,4,5,6];
+
+// {
+// 	0:1,
+// 	1:2,
+// 	2:3,
+// 	3:4,
+// 	4:5,
+// 	5:6,
+// 	length:6
+// } this is what the browser reads from an array
+
+console.log(numArray);
+
+// accesing a specific object in an array using index
+console.log(numArray[0]);
+
+
+// alphabetize an array
+var newNameArr = ['Luke', 'John', 'Mark', 'Angela', 'Matthew', 'Gregory'];
+
+console.log(newNameArr);
+
+console.log(newNameArr.sort());
+
+// numericaly order an array
+newNumArr = [67, 986, 726, 87, 654];
+
+console.log(newNumArr.sort());
+
+console.log(newNumArr.sort(
+	// used function as an argument
+	// runs function(a, b) on every two numbers in the array
+	// and returns the greater of the two
+	function(a, b){
+		return a - b;
+	}))
+
+
+// decending order
+
+var decendingOrder = function(a, b){
+				return b - a;
+}
+
+console.log(newNumArr.sort(decendingOrder))
